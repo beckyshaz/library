c853b73c6d1fec5c6a2647d3c3c796e85a29bb82
beckyshaz <sharonsakah8@gmail.com>
2025-07-27 12:11:00 +0300

    wrote a function for displaying books stored in the array to the library, also used flexbox to enhance the appearance of the books that are displayed on the library for the user

--- script.js
+++ script.js
@@ -6,29 +6,60 @@ function Book(author, title, pages) {
     this.author = author;
     this.title = title;
     this.pages = pages;
-   /* this.save = addBookToLibrary;*/
 
 }
 
 function addBookToLibrary(book) {
-  // take params, create a book then store it in the array
-    /* myLibrary.push({ 
-        id: this.id,
-        author: this.author,
-        title: this.title,
-        pages: this.pages
+    
+    myLibrary.push({ 
+        id: book.id,
+        author: book.author,
+        title: book.title,
+        pages: book.pages
+        
 
-    }); */
-    myLibrary.push(book);
+    }); 
 }
 
-let book = new Book("sharon", "alice in wonderland", 120);
+let book = new Book("Sharon", "alice in wonderland", 120);
+//book.save();
 
 console.log(book);
 
-book1 = new Book("toby", "awesome", 150);
+book1 = new Book("Toby", "Awesome", 150);
+
+book2 = new Book("coby", "Wonderful", 200);
+
+book3 = new Book("Tony", "Blessed", 170);
+
+book4 = new Book("Tobias", "Handsomelly Prosperous", 300);
+
+book5 = new Book("Family", "Blessed Happpy Family", 500);
 
 addBookToLibrary(book1);
+addBookToLibrary(book);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+addBookToLibrary(book4);
+addBookToLibrary(book5);
+console.log(myLibrary.length);
+const container = document.querySelector(".container");
 
-console.log(myLibrary[0]);
+function displayBook () {
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log(myLibrary[i]);
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <p class="title">Title: ${myLibrary[i].title}</p>
+        <p class="author">Author: ${myLibrary[i].author}</p>
+        <p class="pages">Pages: ${myLibrary[i].pages}</p>
+        `
+        div.classList = "book";
+        container.appendChild(div);
+        
+    }
+    
+}
 
+
+displayBook();
